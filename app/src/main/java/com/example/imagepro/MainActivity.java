facbf5b9b3ac289792b7ea67f2fb1dd9c49dccfa
Mohab Baghdad <Mohabbaghdad@gmail.com>
2022-05-11 01:37:40 +0200

    Android App that Acessing Camera using OpenCV

---
+++ app/src/main/java/com/example/imagepro/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.imagepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity: ","Opencv is loaded");
+        }
+        else {
+            Log.d("MainActivity: ","Opencv failed to load");
+        }
+    }
+
+    private Button camera_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickCamera(View v) {
+        Intent i = new Intent(MainActivity.this,CameraActivity.class);
+        startActivity(i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+    }
+}
